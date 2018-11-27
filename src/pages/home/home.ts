77511aff1da0ab5f215d61c8fba097dde6582aa6
JosemarRocha <josemarrochads@gmail.com>
2018-11-27 16:50:13 -0400

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams,  ToastController} from 'ionic-angular';
+import { AngularFireAuth } from "angularfire2/auth"
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private afAuth: AngularFireAuth, private toast: ToastController,
+    
+    public navCtrl: NavController, public navParams: NavParams) {
 
   }
 
+  ionViewWillLoad(){
+    this.afAuth.authState.subscribe(data => {
+      if(data && data.email && data.uid){
+        this.toast.create({
+          message: 'Welcome to Lembrol, '+data.email,
+          duration: 3000
+        }).present();
+      }else{
+        this.toast.create({
+          message: 'Could not find auth info',
+          duration: 3000
+        }).present();
+      }
+    });
+  }
 }
