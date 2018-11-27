59230ae2d726405f0e88d05843014cd5af74635d
JosemarRocha <josemarrochads@gmail.com>
2018-11-27 16:47:23 -0400

    Add files via upload

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDi7OqLeMXXxpOfsFStQckcr_pVrJANPzk",
+    authDomain: "fir-auth-6e83f.firebaseapp.com",
+    databaseURL: "https://fir-auth-6e83f.firebaseio.com",
+    projectId: "fir-auth-6e83f",
+    storageBucket: "fir-auth-6e83f.appspot.com",
+    messagingSenderId: "289368064439"
+};
