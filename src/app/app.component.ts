f018ccfe8900f97156ce4f23107a9ecd38085364
JosemarRocha <josemarrochads@gmail.com>
2018-11-27 16:48:05 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { ConfigPage } from './../pages/config/config';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login'
 
 @Component({
   templateUrl: 'app.html'
@@ -13,7 +14,7 @@ import { ListPage } from '../pages/list/list';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = LoginPage;
 
   pages: Array<{title: string, component: any}>;
 
