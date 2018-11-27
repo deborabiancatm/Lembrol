0185c98da8af48a0de75db9cc1cd8cef994e3ef8
JosemarRocha <josemarrochads@gmail.com>
2018-11-27 16:49:45 -0400

    Add files via upload

---
+++ src/pages/register/register.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { User } from '../../models/user';
+import { AngularFireAuth } from "angularfire2/auth"
+import { LoginPage } from '../login/login';
+
+@IonicPage()
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html',
+})
+export class RegisterPage {
+
+  user = {} as User;
+
+  constructor(private afAuth: AngularFireAuth,
+    
+    public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  async register(user: User){
+    try{
+      const result = await this.afAuth.auth.createUserAndRetrieveDataWithEmailAndPassword(user.email, user.password);
+      console.log(result);
+      this.navCtrl.popTo(LoginPage);
+    }
+    catch (e) {
+      console.error(e);
+    }
+  }
+}
