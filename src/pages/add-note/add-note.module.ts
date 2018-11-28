c3b134f4da00e3d219eebf28c58f5ccd29a5f52b
JosemarRocha <josemarrochads@gmail.com>
2018-11-28 11:31:23 -0400

    Update add-note.module.ts

--- src/pages/add-note/add-note.module.ts
+++ src/pages/add-note/add-note.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { AddNotePage } from './add-note';
+import { AddNotePage } from '../add-note/add-notes';
 
 @NgModule({
   declarations: [
