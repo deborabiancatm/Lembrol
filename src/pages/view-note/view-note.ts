1c4af9cc194b611f5c682bebd0515645982e52fd
JosemarRocha <josemarrochads@gmail.com>
2018-11-28 11:35:00 -0400

    Update view-note.ts

--- src/pages/view-note/view-note.ts
+++ src/pages/view-note/view-note.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { NoteService } from '../../providers/note-service/note-service';
 import { Note } from '../../models/note.model';
 
-@IonicPage()
 @Component({
   selector: 'page-view-note',
   templateUrl: 'view-note.html',
